426afd89c7c75bb28b23d9af8ab27e348f074a63
sodino <sodino@qq.com>
2013-08-17 18:06:13 +0800

    随便写的一点点，存一下..

--- src/lab/sodino/sounddemo/RecordActivity.java
+++ src/lab/sodino/sounddemo/RecordActivity.java
@@ -1,17 +1,30 @@
 package lab.sodino.sounddemo;
 
+import java.io.File;
+
 import lab.sodino.sounddemo.sound.SoundRecorder;
+import lab.sodino.sounddemo.sound.SoundRecorder.SoundRecordListener;
 import android.app.Activity;
+import android.graphics.drawable.ClipDrawable;
 import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.Handler.Callback;
+import android.os.Message;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
 import android.widget.Button;
+import android.widget.ImageView;
 
-public class RecordActivity extends Activity {
+public class RecordActivity extends Activity implements SoundRecordListener, Callback{
+	public static final int MSG_RECORD = 1;
 	private SoundRecorder soundRecorder;
-	
+	private ImageView imgMicroPhone;
+	private ClipDrawable clipDrawable;
+	private Handler handler;
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_record);
@@ -24,8 +37,9 @@ public class RecordActivity extends Activity {
 					int action = event.getAction();
 					if(action == MotionEvent.ACTION_DOWN){
 						btnPressed2Talk.setText(R.string.released2finish);
-						
-						soundRecorder = new SoundRecorder("/sdcard/test.amr");
+						String path = Environment.getExternalStorageDirectory() +File.separator + "test.amr";
+						soundRecorder = new SoundRecorder(path);
+						soundRecorder.addSoundRecordListener(RecordActivity.this);
 						soundRecorder.startRecord();
 					}else if(action == MotionEvent.ACTION_UP || action == MotionEvent.ACTION_CANCEL){
 						btnPressed2Talk.setText(R.string.pressed2talk);
@@ -36,5 +50,39 @@ public class RecordActivity extends Activity {
 				return false;
 			}
 		});
+		
+		imgMicroPhone = (ImageView) findViewById(R.id.imgMicroPhone);
+		clipDrawable = (ClipDrawable) imgMicroPhone.getDrawable();
+		
+		handler = new Handler(this);
+	}
+
+	@Override
+	public void recordStart() {
+		
+	}
+
+	@Override
+	public void recordProgress(int maxAmplitude) {
+		Log.d("ANDROID_LAB", "maxAmp="+maxAmplitude);
+		Message msg = handler.obtainMessage();
+		msg.what = MSG_RECORD;
+		msg.arg1 = maxAmplitude;
+		handler.sendMessage(msg);
+	}
+
+	@Override
+	public void recordComplete(int state) {
+		
+	}
+
+	@Override
+	public boolean handleMessage(Message msg) {
+		switch(msg.what){
+		case MSG_RECORD:
+			clipDrawable.setLevel(msg.arg1);	
+			break;
+		}
+		return false;
 	}
 }
--- src/lab/sodino/sounddemo/sound/SoundRecorder.java
+++ src/lab/sodino/sounddemo/sound/SoundRecorder.java
@@ -1,13 +1,22 @@
 package lab.sodino.sounddemo.sound;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import android.media.MediaRecorder;
 
 public class SoundRecorder {
+	public static final int STATE_INIT = 0;
+	public static final int STATE_RECORDING = 1;
+	public static final int STATE_COMPLETE = 2;
+	public static final int STATE_ERROR = 3;
 	
+	public static final int  SLEEP_TIME = 10;
 	private MediaRecorder mediaRecorder;
-	
+	private ArrayList<SoundRecordListener> arrListener = new ArrayList<SoundRecorder.SoundRecordListener>();
+	private boolean recording = true;
+	private RecordThread recordThread;
+	private int state;
 	public SoundRecorder(String path){
 		mediaRecorder = new MediaRecorder();
 		// 设置声音来源为麦克风
@@ -18,22 +27,65 @@ public class SoundRecorder {
 		mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
 		// 设置存放路径
 		mediaRecorder.setOutputFile(path);
+		
+		state = STATE_INIT;
 	}
 	
 	public void startRecord(){
-		try {
-			mediaRecorder.prepare();
-			mediaRecorder.start();
-		} catch (IllegalStateException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
+		if(recordThread==null){
+			recordThread = new RecordThread();
+			recordThread.start();
 		}
 	}
 	
 	public void stopRecord(){
-		mediaRecorder.stop();
-		mediaRecorder.release();
-		mediaRecorder = null;
+		recording = false;
+	}
+
+	public void addSoundRecordListener(SoundRecordListener lis){
+		if(lis != null && arrListener.contains(lis) == false){
+			arrListener.add(lis);
+		}
+	}
+	
+	class RecordThread extends Thread{
+		RecordThread(){
+			setName("RecordThread");
+		}
+		public void run(){
+			try {
+				mediaRecorder.prepare();
+				mediaRecorder.start();
+			} catch (IllegalStateException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			state = STATE_RECORDING;
+			while (recording) {
+				int maxAmplitude = mediaRecorder.getMaxAmplitude();
+				for(SoundRecordListener lis:arrListener){
+					lis.recordProgress(maxAmplitude);
+				}
+				try {
+					Thread.sleep(SLEEP_TIME);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			mediaRecorder.stop();
+			mediaRecorder.release();
+			mediaRecorder = null;
+			for(SoundRecordListener lis:arrListener){
+				lis.recordComplete(state);
+			}
+			state = STATE_COMPLETE;
+		}
+	}
+	
+	public static interface SoundRecordListener{
+		public void recordStart();
+		public void recordProgress(int maxAmplitude);
+		public void recordComplete(int state);
 	}
 }
